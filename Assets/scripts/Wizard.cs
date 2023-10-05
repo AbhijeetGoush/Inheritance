6ffc871a9d41503584114c41fd45d515535b47f1
AbhijeetGoush <530082@chrome.escg.ac.uk>
2023-10-05 10:11:31 +0100

    completed

---
+++ Assets/scripts/Wizard.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wizard : Enemy
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        AttackPlayer();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
